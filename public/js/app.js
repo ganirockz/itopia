c55ebbd3e29fc31aa49fa7aaf2b28c5ad2e2b349
raghu <raghu@iRepairs-MacBook-Air.local>
2015-05-09 10:05:49 +0530

    mongo post get done

--- public/js/app.js
+++ public/js/app.js
@@ -1,29 +1,17 @@
 angular.module('itopia', [ ])
 .controller('appctrl', ['$scope', '$http', function ($scope, $http) {
   //console.log('hello world!');
-  var post1 = {
-    title : "Give away of bycycle",
-    description : "in very good condition, suitable for kids by the age of 6 to 10",
-    lat : 12.971599,
-    long : 77.594563,
-    ts :  moment()
-  };
+  var now = moment();
+  $http.get('/itopia').success(function (res) {
+    $scope.posts = res;
+  });
 
-  var post2 = {
-    title : "Give away of furniture",
-    description : "in very good condition, suitable for kids by the age of 6 to 10",
-    lat : 12.971599,
-    long : 77.594563,
-    ts :  moment()
-  };
-
-  $scope.posts = [post1, post2];
-
-
-
-
-
-
-
-}]);
+  $scope.addPost = function (post) {
+    //console.log(post);
+    $http.post('/itopia', post).success(function (res) {
+      console.log(res);
+      $scope.posts = res;
+    })
+  }
+  }]);
 ;
